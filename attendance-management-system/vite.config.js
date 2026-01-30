ac96adc24c942b220d697d35d97e70837107910c
ially4198 <106965610+ially4198@users.noreply.github.com>
2026-01-30 21:16:37 +0000

    fixed route issue

--- attendance-management-system/vite.config.js
+++ attendance-management-system/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-    outDir: 'dist'
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
